6239ba603236eb1781dcbca379afd66055d5d623
BrunoQ <bruno.8363781@edu.univali.br>
2024-10-01 17:45:00 -0300

    third commit

--- src/components/Noticias/Noticia02/Noticia02.tsx
+++ src/components/Noticias/Noticia02/Noticia02.tsx
@@ -8,7 +8,7 @@ export function Noticia02() {
         <figure className="w-[320px] hover:cursor-pointer group text-black">
           <img src={noticia02} alt="" className="w-[320px]" />
           <figcaption className="text-xl font-bold group-hover:underline pt-2 pb-4">
-            Quem é Demetrian Titus? O salvador do sistema Recidious.
+            Quem é Demetrian Titus? O salvador do sistema solar Recidious.
           </figcaption>
           <p>Tenente Demetrian Titus, um dos mais valentes Ultramarinos. Quem ele é?</p>
         </figure>
